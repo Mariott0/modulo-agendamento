a555374335b809f1c2ab13b42557e529fc772293
Mariott0 <loucosgamer124@gmail.com>
2025-04-29 20:02:41 -0300

    update 29/04/2025

--- angular-modulo-agendamento/src/app/profissional/listagem/listagem.component.ts
+++ angular-modulo-agendamento/src/app/profissional/listagem/listagem.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProfissionalService } from '../profissional.service';
+import { Profissional } from '../profissional.model';
 
 @Component({
   selector: 'app-listagem',
@@ -6,6 +8,18 @@ import { Component } from '@angular/core';
   templateUrl: './listagem.component.html',
   styleUrl: './listagem.component.css'
 })
-export class ListagemComponent {
+export class ListagemComponent implements OnInit {
 
+  profissionais: Profissional[] = [];
+
+  constructor(private profissionalService: ProfissionalService) { }
+
+  ngOnInit(): void {
+
+  }
+  carregarProfissionais(): void {
+    this.profissionalService.listarProfissionais().subscribe((res) => {
+      this.profissionais = res;
+    })
+  }
 }
