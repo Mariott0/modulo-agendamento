f59a6ae9d060ec46fb0102c2592c7cc8b9b264fd
Mariott0 <loucosgamer124@gmail.com>
2025-06-11 14:12:09 -0300

    Update 11/06/2025

---
+++ api-modulo-agendamento/prisma/migrations/20250611170311_alteracao_telefone_string/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "profissional" ALTER COLUMN "telefone" SET DATA TYPE TEXT;
--- api-modulo-agendamento/src/profissional/dto/create-profissional.dto.ts
+++ api-modulo-agendamento/src/profissional/dto/create-profissional.dto.ts
@@ -3,9 +3,13 @@ import { IsInt, IsString } from 'class-validator';
 export class CreateProfissionalDto {
   @IsString()
   id: string;
+  @IsString()
   nome: string;
+  @IsString()
   telefone: string;
+  @IsString()
   endereco: string;
+  @IsString()
   especialidade: string;
   @IsInt()
   idade: number;
